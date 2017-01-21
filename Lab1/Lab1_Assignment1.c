d8ed4169589fe1e3b8fef6ae8570a2277bfdb40a
alfran <2015kucp1032@iiitkota.ac.in>
2017-01-21 14:50:04 +0530

    changes added

--- Lab1/Lab1_Assignment1.c
+++ Lab1/Lab1_Assignment1.c
@@ -3,6 +3,9 @@
 #include<ctype.h>
 int main()
 {
+
+/*Version1 : Static*/
+
 /*Processor type*/
 FILE *fp;
 
@@ -32,4 +35,47 @@ printf("\n\n*************************\n\n");
 /*Amount of time since the system was last booted*/
 printf("System was last booted since:\n");
 system("cat /proc/uptime");
+
+
+
+
+
+/*Version2 : Dynamic*/
+
+
+/*The percentage of time the processor(s) spend in user mode, system mode, and
+
+the percentage of time the processor(s) are idle*/
+
+float sum,j;
+/*fp = fopen("/proc/stat","r");
+fscanf(fp,"%s",s);
+for(i=0;i<)*/
+
+/*The amount and percentage of available (or free) memory*/
+
+fp = fopen("/proc/meminfo","r");
+
+fscanf(fp,"%s",s);//Taking the string MemTotal
+fscanf(fp,"%s",s); // taking the number in string format
+j = (float)atoi(s); //converting the string to integer and typecasting it to float
+fscanf(fp,"%s",s); // for taking kB
+
+fscanf(fp,"%s",s);//taking the string MemFree
+fscanf(fp,"%s",s); // taking the number in string format
+
+sum = (float)atoi(s);//converting the string to integer and typecasting it to float
+
+printf("Amount of free memory is :%.2f \nPercentage of available memory is:%.2f \n",sum,(sum/j)*100);
+
+fclose(fp);
 }
+
+
+
+
+
+
+
+
+
