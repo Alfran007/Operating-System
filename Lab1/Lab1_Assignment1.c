a2483c8605ba8f63dce593c8513858d36fb53963
alfran <2015kucp1032@iiitkota.ac.in>
2017-01-21 22:09:16 +0530

    almost done

--- Lab1/Lab1_Assignment1.c
+++ Lab1/Lab1_Assignment1.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<ctype.h>
+#include<unistd.h>
 int main(int argc, char * argv[])
 {
 
@@ -26,10 +27,10 @@ return 1;
 }
 
 /* Declaration of Vriables*/
+int i=0;
+float sum=0,j,a[1000],percent=0,sum3=0,User = 0,System=0,Idle=0,sum1=0,sum2=0,count=0,sum_i=0,sum_p=0,p=0,f=0; 
+char s[1000];
 
-int sum1=0,sum2=0,i=0;
-float sum=0,j,a[1000]; 
-char b[10000],s[1000];
 
 FILE *fp; //Declaring a file pointer
 
@@ -76,31 +77,41 @@ if(argc==2)
 else
 {
 /*Version2 : Dynamic*/
-printf("\n\n*************************\n\n");
+//printf("\n\n*************************\n\n");
 int m =atoi(argv[1]);
 int n = atoi(argv[2]);
-int k = n/m;
-printf("k is :%d\n",k);
+int k = n/m,o=k;
+//printf("k is :%d\n",k);
+//while(1)
+{
+sum1=0;
+sum2=0;
 while(k--)
 {
 
 /*The percentage of time the processor(s) spend in user mode, system mode, and
 the percentage of time the processor(s) are idle*/
 
-
+sum3 =0;
 fp = fopen("/proc/stat","r");
 fscanf(fp,"%s",s);
 for(i=0;i<10;i++)
 {
-fscanf(fp,"%f",&a[i]);
-sum = sum + a[i];
+fscanf(fp,"%s",s);
+a[i] = (float)atoi(s);
+//printf("a[%d] is %f\n",i,a[i]);
+sum3 = sum3 + a[i];
 }
-printf("User Mode:  %.2f\n",(a[0]/sum)*100);
-printf("System Mode:  %.2f\n",(a[2]/sum)*100);
-printf("IdleMode:  %.2f\n",(a[3]/sum)*100);
 
+User = User + (a[0]/sum3)*100;
+System = System + (a[2]/sum3)*100 ;
+Idle = Idle + (a[3]/sum3)*100 ;
+
+
+
+fclose(fp);
 /*The amount and percentage of available (or free) memory*/
-printf("\n\n*************************\n\n");
+//printf("\n\n*************************\n\n");
 
 
 fp = fopen("/proc/meminfo","r");
@@ -112,65 +123,88 @@ fscanf(fp,"%s",s); // for taking kB
 
 fscanf(fp,"%s",s);//taking the string MemFree
 fscanf(fp,"%s",s); // taking the number in string format
+sum = sum + (float)atoi(s);//converting the string to integer and typecasting it to float
+percent = percent + ((float)atoi(s)/j)*100;
 
-sum = (float)atoi(s);//converting the string to integer and typecasting it to float
-
-printf("Amount of free memory is :%.2f \nPercentage of available memory is:%.2f \n",sum,(sum/j)*100);
 
 fclose(fp);
 
 /* The rate (number of blocks per second) of disk read/write in the system*/
-printf("\n\n*************************\n\n");
+//printf("\n\n*************************\n\n");
 
 fp = fopen("/proc/diskstats","r");
-while(!feof(fp))
-{
-while(1)
-{
-fscanf(fp,"%c",&b[i]);
+f=0;
+//while(1)
+//{
+//fscanf(fp,"%c",&b[f]);
+//printf("%c",b[f]);
+//if(b[f]=='s')
+	for(f=0;f<3;f++)
+	{
+	fscanf(fp,"%s",s);
+	}
+	fscanf(fp,"%s",s);
 
-if(b[i]=='s')
-{
-	fscanf(fp,"%s",b);
-	fscanf(fp,"%s",b);
+	sum1 = sum1 + atoi(s); 
 
-	sum1 = sum1 + atoi(b); 
+	for(f=0;f<3;f++)
+	{
+	fscanf(fp,"%s",s);
+	}
+	fscanf(fp,"%s",s);
 
-	fscanf(fp,"%s",b);
-	fscanf(fp,"%s",b);
-	fscanf(fp,"%s",b);
-	fscanf(fp,"%s",b);
+	sum2 = sum2 + atoi(s);
 
-	sum2 = sum2 + atoi(b);
-}
-break;
-i++;
-}
-}
-printf("\nDisk Read: %d\nDisc Write: %d\n",sum1,sum2);
+
+
 
 fclose(fp);
 
-printf("\n\n*************************\n\n");
+//
 
 /*The rate (number per second) of context switches in the kernel*/
 fp = popen("cat /proc/stat | grep 'ctxt' | uniq","r");
 fscanf(fp,"%s",s);
 fscanf(fp,"%s",s);
 i = atoi(s);
-printf("Context Switches: %d\n",i);
+sum_i = sum_i + i;
+
 
 
 /*The rate (number per second) of process creations in the system*/
 fp = popen("cat /proc/stat | grep 'processes' | uniq","r");
 fscanf(fp,"%s",s);
 fscanf(fp,"%s",s);
-i = atoi(s);
-printf("Processes: %d\n",i);
+p = atoi(s);
+sum_p = p + sum_p;
+
+sleep(m);
+count++;
+}
+
+
+printf("\n\n*************************\n\n");
+printf("User Mode:  %.2f\n",User/(float)o);
+printf("\n\n*************************\n\n");
+printf("System Mode:  %.2f\n",System/(float)o);
+printf("\n\n*************************\n\n");
+printf("IdleMode:  %.2f\n",Idle/(float)o);
+printf("\n\n*************************\n\n");
+printf("Amount of free memory is :%.2f \nPercentage of available memory is:%.2f \n",sum/(float)o,percent/(float)o);
+printf("\n\n*************************\n\n");
+printf("\nDisk Read: %.2f\nDisk Write: %.2f\n",sum1/o,sum2/o);
+printf("\n\n*************************\n\n");
+printf("Context Switches: %.0f\n",sum_i/o);
+printf("\n\n*************************\n\n");
+printf("Processes: %.0f\n",sum_p/o);
+printf("\n\n*************************\n\n");
 }
 
 }
+
+
 }
+
 return 0;
 }
 
