ea0ba03b5586249e1e1f3647a8bae778d1c5180e
alfran <2015kucp1032@iiitkota.ac.in>
2017-01-21 23:44:17 +0530

    Completed

--- Lab1/Lab1_Assignment1.c
+++ Lab1/Lab1_Assignment1.c
@@ -5,30 +5,42 @@
 int main(int argc, char * argv[])
 {
 
+
+/* Error Handling */
 if(argc==3)
 {
+char c = argv[1][0];
+if((c<='z' && c>='a') || (c<='Z' && c>='A'))
+{
+printf("Error: Please Enter possitive numbers\n"); // Checking for first argument not to be an alphabet.
+return 1;
+}
+c = argv[2][0];
+if((c<='z' && c>='a') || (c<='Z' && c>='A'))
+{
+printf("Error: Please Enter possitive numbers\n"); // Checking for second argument not to be an alphabet.
+return 1;
+}
 int m =atoi(argv[1]);
 int n = atoi(argv[2]);
 
-/* Error Handling */
-
 if(m<=0 || n<=0)
 {
-printf("Error: Enter only possitive values\n");
+printf("Error: Enter only possitive values\n");  //Checking the numbers to be possitive.
 return 1;
 }
 
 
 if(n%m!=0)
 {
-printf("Error: Enter n as a multiple of m\n");
+printf("Error: Enter n as a multiple of m\n"); //Checking the second number should be divided by first.
 return 1;
 }
 }
 
 /* Declaration of Vriables*/
-int i=0;
-float sum=0,j,a[1000],percent=0,sum3=0,User = 0,System=0,Idle=0,sum1=0,sum2=0,count=0,sum_i=0,sum_p=0,p=0,f=0; 
+int i=0,count=0;
+float sum=0,j,a[1000],percent=0,sum3=0,User = 0,System=0,Idle=0,sum1=0,sum2=0,sum_i=0,sum_p=0,p=0,f=0; 
 char s[1000];
 
 
@@ -77,21 +89,24 @@ if(argc==2)
 else
 {
 /*Version2 : Dynamic*/
-//printf("\n\n*************************\n\n");
+
 int m =atoi(argv[1]);
 int n = atoi(argv[2]);
 int k = n/m,o=k;
-//printf("k is :%d\n",k);
-//while(1)
+
+while(1)
 {
 sum1=0;
 sum2=0;
-while(k--)
+k = n/m;
+if(k<=0)
+break;
+while(k>0)
 {
-
 /*The percentage of time the processor(s) spend in user mode, system mode, and
 the percentage of time the processor(s) are idle*/
-
+if(k<=0)
+break;
 sum3 =0;
 fp = fopen("/proc/stat","r");
 fscanf(fp,"%s",s);
@@ -99,7 +114,6 @@ for(i=0;i<10;i++)
 {
 fscanf(fp,"%s",s);
 a[i] = (float)atoi(s);
-//printf("a[%d] is %f\n",i,a[i]);
 sum3 = sum3 + a[i];
 }
 
@@ -107,11 +121,9 @@ User = User + (a[0]/sum3)*100;
 System = System + (a[2]/sum3)*100 ;
 Idle = Idle + (a[3]/sum3)*100 ;
 
-
-
 fclose(fp);
 /*The amount and percentage of available (or free) memory*/
-//printf("\n\n*************************\n\n");
+
 
 
 fp = fopen("/proc/meminfo","r");
@@ -126,19 +138,12 @@ fscanf(fp,"%s",s); // taking the number in string format
 sum = sum + (float)atoi(s);//converting the string to integer and typecasting it to float
 percent = percent + ((float)atoi(s)/j)*100;
 
-
 fclose(fp);
 
 /* The rate (number of blocks per second) of disk read/write in the system*/
-//printf("\n\n*************************\n\n");
 
 fp = fopen("/proc/diskstats","r");
-f=0;
-//while(1)
-//{
-//fscanf(fp,"%c",&b[f]);
-//printf("%c",b[f]);
-//if(b[f]=='s')
+
 	for(f=0;f<3;f++)
 	{
 	fscanf(fp,"%s",s);
@@ -155,12 +160,8 @@ f=0;
 
 	sum2 = sum2 + atoi(s);
 
-
-
-
 fclose(fp);
 
-//
 
 /*The rate (number per second) of context switches in the kernel*/
 fp = popen("cat /proc/stat | grep 'ctxt' | uniq","r");
@@ -168,7 +169,7 @@ fscanf(fp,"%s",s);
 fscanf(fp,"%s",s);
 i = atoi(s);
 sum_i = sum_i + i;
-
+fclose(fp);
 
 
 /*The rate (number per second) of process creations in the system*/
@@ -177,11 +178,13 @@ fscanf(fp,"%s",s);
 fscanf(fp,"%s",s);
 p = atoi(s);
 sum_p = p + sum_p;
+fclose(fp);
 
 sleep(m);
-count++;
-}
+k--;
 
+}
+count++;
 
 printf("\n\n*************************\n\n");
 printf("User Mode:  %.2f\n",User/(float)o);
@@ -197,14 +200,10 @@ printf("\n\n*************************\n\n");
 printf("Context Switches: %.0f\n",sum_i/o);
 printf("\n\n*************************\n\n");
 printf("Processes: %.0f\n",sum_p/o);
-printf("\n\n*************************\n\n");
+printf("\n\n*************************....(%d)\n\n",count);
 }
-
 }
-
-
 }
-
 return 0;
 }
 
