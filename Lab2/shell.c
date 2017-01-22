a9ae2e5d7a37aeb08ff1e693bb1b0ae5e3776deb
alfran <2015kucp1032@iiitkota.ac.in>
2017-01-22 20:05:40 +0530

    Shell Added

---
+++ Lab2/shell.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stddef.h>
+#include <sys/types.h>
+#include<sys/wait.h>
+int main(void)
+{
+
+char *s,*t,*in;
+char *arr[1000];
+pid_t pd;
+int i;
+size_t size;
+printf("*********Alfran shell executes************\n");
+while(1)
+{
+
+s=malloc(100*sizeof(char));
+printf("\nroot@alfran_shell:~");
+getline(&s,&size,stdin);
+s[strlen(s)-1] = '\0';
+
+
+if(!strcmp(s,"exit\n"))
+{
+	exit(0); // Exit on exit
+}
+if(feof(stdin))  // Exit on Ctrl + D
+{ 
+printf("\n");
+exit(0);
+}
+
+
+t = strtok(s," ");
+arr[0] = t;
+for(i=1; t!=NULL ;i++)	
+{
+t = strtok(NULL, " ");
+arr[i] = t;
+}
+
+
+if(i>0)
+{
+pd = fork();
+if(pd<0)
+{
+printf("Error: Child process forking failed");
+}
+if(pd>=0)
+{
+if(pd==0)
+{
+	if(execvp(arr[0],arr)<0)
+	{
+		printf("Error: Command not found\n");
+	}
+}
+else
+	{
+		wait(NULL);
+	}
+
+}
+}
+}
+
+}
