6c11bbab70d7553fe1844583cfeb40704a04c95e
alfran <2015kucp1032@iiitkota.ac.in>
2017-01-24 00:06:02 +0530

    final_shell

--- Lab2/alfranshell.c
+++ Lab2/alfranshell.c
@@ -15,162 +15,131 @@ char *arr[1000];
 
 int check(char *token[], char *ch)
 {
-
-//if(t==NULL || token[0]==NULL)
-//return 0;
-if(i>0)
-{
-/*if(flag==1)
-{
-waitpid(pd,&status,WUNTRACED);
-return 0;
-}*/
-//printf("\n%s\n",token[0]);
-if(!strcmp(token[0],"cd"))  // program for cd command.
-{
-
-inp = strtok(NULL," ");
-
-//printf("input is %s ",inp);
-//printf("\n%sasd\n",t);
-	if(inp==NULL)
-	{
-	//chdir("/home");
-	return 0;
-	}
-
-	else if(chdir(inp)==-1) //checking valid directory.
-	{
-	printf("Error: Not a valid directory\n");  
-	return 0;  
-	}
-return 0;
-}
-
-pd = fork();  // getting the process id and forking the program.
-if(pd<0)
-{
-      printf("Error: Child process forking failed\n");
-	
-	      return 0;
-      }
-else  if(pd==0)
+      if(i>=0)
+      {
+            if(!strcmp(token[0],"cd"))  // program for cd command.
             {
-	            if(execvp(token[0],token)<0)
+
+                  inp = strtok(NULL," ");
+	            if(inp==NULL)
 	            {
-		            printf("Error: Command not found\n");
+	                return 0;
 	            }
-	            exit(0);
+
+	            else if(chdir(inp)==-1) //checking valid directory.
+	            {
+	                  printf("Error: Not a valid directory\n");  
+	                  return 0;  
+	            }
+	            printf("directory changed\n");
+                  return 0;
+            }
+
+            pd = fork();  // getting the process id and forking the program.
+            if(pd<0)
+            {
+                  printf("Error: Child process forking failed\n");
+	
+	            return 0;
+            }
+            else  if(pd==0)
+            {
+                  if(execvp(token[0],token)<0)
+                  {
+                        printf("Error: Command not found\n");
+                  }
+                  exit(0);
             }
             else
-	            {
-		            if(strcmp(ch,"&"))
-		            {
-			            waitpid(pd,NULL,0);	
-		            }
+            {
+                  if(strcmp(ch,"&"))
+                  {
+                        waitpid(pd,NULL,0);	
+                  }
 
-	            }         
+            }         
+
+      }
 
 }
 
-}
-
-int main(void)
+int main(int argc,char *argv[])
 {
-int k;
-
-//void ampersand();
-printf("*********Alfran shell executes************\n");
-
-
-while(1)
-{
-flag =0;
-s=malloc(100*sizeof(char));
-printf("\nroot@alfran_shell:~");
-getline(&s,&size,stdin);
-s[strlen(s)-1] = '\0';
+      int k;
+      int j=0;
+      printf("*********Alfran shell executes************\n");
 
 
-if(!strcmp(s,"exit"))
-{
-      printf("\nThanks for using Alfran's Shell! :)\n"); 
-	exit(0);	// Exit on exit
+      while(1)
+      {
+            flag =0;
+            s=malloc(100*sizeof(char));
+            printf("\nroot@alfran_shell:~");
+            getline(&s,&size,stdin);
+            s[strlen(s)-1] = '\0';
+
+
+            if(!strcmp(s,"exit"))
+            {
+                  printf("\nThanks for using Alfran's Shell! :)\n"); 
+	            exit(0);	// Exit on exit
 	
-}
-if(feof(stdin))  // Exit on Ctrl + D
-{ 
-printf("\n");
-printf("\nThanks for using Alfran's Shell! :)\n"); 
-exit(0);
-}
+            }
+            if(feof(stdin))  // Exit on Ctrl + D
+            { 
+                  printf("\n");
+                  printf("\nThanks for using Alfran's Shell! :)\n"); 
+                  exit(0);
+            }
 
-i=0;
-t = strtok(s," ");  // taking string before space.
-//printf("%s ",t);
-arr[i] = t;
-if(!strcmp(t,"cd"))
-{
-	//printf("cd typed!\n");
-	check(arr,"cd");
-	//if(x==0)
-	  //    continue;
-}
-int j=0;
-for(i=2; t!=NULL ;i++)	
-{
-flag =0;
-outp = t;
-t = strtok(NULL," ");
-
-//printf("it is :%s ",t);
-  // taking the string parts breaking through spaces.
-//printf("hereds token is %s\n",t);
+            i=0;
+            j=0;
+            t = strtok(s," ");  // taking string before space.
+            arr[i] = t;
+            if(!strcmp(arr[i],"cd"))
+            {
+	            check(arr+j,"cd");    
+            }
+            
+            for(i=2; t!=NULL ;i++)	
+            {
+                  flag =0;
+                  outp = t;
+                  t = strtok(NULL," ");
+                  
 
 
-if(t==NULL)
-{
-      
-	check(arr+j," ");
-	//j=i+1;
-	continue;
-}
-arr[i-1] = t;
-if(!strcmp(t,"&"))
-{
-//	printf("here token is %s\n",t);
-	arr[i-1]=NULL;
-	//for(k=0;k<=i;k++)
-      //	printf("%s ",arr[k]);
-	check(arr+j,"&");
-	//arr[i-1]="&";
-	
-	j=i;
-	continue;
-}
+                  if(t==NULL)
+                  {
+                        
+                        arr[i-1] = NULL;
+	                  check(arr+j," ");
+	                  arr[i-1]=t;
+	                  j =i;
+	                  continue;
+                  }
+                  arr[i-1] = t;
+                  if(!strcmp(arr[i-1],"&"))
+                  {
+	                  arr[i-1]=NULL;
+	                  check(arr+j,"&");
+	                  arr[i-1]="&";
+	                  j=i;
+	                  break;
+                  }
 
-else if(!strcmp(t,"|"))
-{
-	check(arr+j,"|");
-	j=i;
-	continue;
-}
-else if(!strcmp(t,"cd"))
-{
-	//printf("cd typed!\n");
-	check(arr+j,"cd");
-	continue;
-	//if(x==0)
-	  //    continue;
-}
-else
-{
-      //check(arr+j," ");
-}
+                  else if(!strcmp(t,"|"))
+                  {     
+	                  check(arr+j,"|");
+	                  j=i;
+	                  continue;
+                  }
+             
 
-}
-}
-printf("\nThanks for using Alfran's Shell! :)\n");
+            }
+      }
+      printf("\nThanks for using Alfran's Shell! :)\n");
+      return 0;
 
 }
 
